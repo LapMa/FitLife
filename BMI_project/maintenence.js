d09cc860970334ff47c2f993fa161f09e07a03fa
LapMa <90623614+LapMa@users.noreply.github.com>
2023-12-15 11:27:28 +0100

    Delete BMI_project directory

--- BMI_project/maintenence.js
+++
@@ -1,8 +0,0 @@
-function maintenence()
-{
-    let heightInput = document.getElementById('height');
-    let weightInput = document.getElementById('weight');
-    let ageInput = document.getElementById('age');
-
-}
-    
